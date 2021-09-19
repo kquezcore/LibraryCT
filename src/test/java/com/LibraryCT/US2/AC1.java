faa0a2d525e88760d58c158e78c87196eb77a252
karina quezada <quezadacore32@gmail.com>
2021-09-19 00:13:02 -0400

    US2

---
+++ src/test/java/com/LibraryCT/US2/AC1.java
@@ -0,0 +1,18 @@
+package com.LibraryCT.US2;
+
+public class AC1 {
+
+    /*
+    As a librarian, I should be able to add a new user.
+AC #1:
+Given librarian is on the homePage
+When librarian click Users module
+And librarian click “+Add User” button
+When librarian enter full name, password, email and
+address
+And librarian click save changes
+Then verify a new user is created
+Note: To add new user, the date part automation is not covered
+yet, you can skip it
+     */
+}
