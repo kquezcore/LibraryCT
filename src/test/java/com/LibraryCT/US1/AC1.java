dc91a1c39be6f6bb197138d7700a2668d655b0d3
karina quezada <quezadacore32@gmail.com>
2021-09-19 00:12:53 -0400

    US1

---
+++ src/test/java/com/LibraryCT/US1/AC1.java
@@ -0,0 +1,19 @@
+package com.LibraryCT.US1;
+
+import com.LibraryCT.utility.TestBase;
+
+
+public class AC1 extends TestBase {
+
+    /*
+     AC #1:
+Given librarian is on the loginPage
+Then verify that the title is “Login - Library”
+When librarian enters valid email address and password
+And librarian click sign in button
+Then verify that there are 3 models on the page
+     */
+
+
+
+}
