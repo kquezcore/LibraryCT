1e8f8356c948527859e6725fc5d3a796c76ebb87
karina quezada <quezadacore32@gmail.com>
2021-09-19 00:13:08 -0400

    US3

---
+++ src/test/java/com/LibraryCT/US3/AC1.java
@@ -0,0 +1,15 @@
+package com.LibraryCT.US3;
+
+public class AC1 {
+    /*
+    User Story 3
+As a librarian, I should be able to add a new book.
+AC #1:
+Given librarian is on the homePage
+When librarian click Books module
+And librarian click “+Add Book” button
+When librarian enter BookName, ISBN, Year, Author, and Description
+And librarian click save changes
+Then verify a new book is added
+     */
+}
