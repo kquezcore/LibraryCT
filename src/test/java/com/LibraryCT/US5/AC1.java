e12765d0357f8f5370a1dd2506062798bcbb6a6b
karina quezada <quezadacore32@gmail.com>
2021-09-19 00:13:26 -0400

    US5

---
+++ src/test/java/com/LibraryCT/US5/AC1.java
@@ -0,0 +1,14 @@
+package com.LibraryCT.US5;
+
+public class AC1 {
+
+    /*
+    User Story 5
+As a librarian, I want to select user groups.
+AC #1:
+Given librarian is on the homePage
+When librarian click Users module
+And librarian click user group dropdown
+Then verify librarian have 3 options
+     */
+}
