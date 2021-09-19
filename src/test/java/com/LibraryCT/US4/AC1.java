a392c055f5744d65a51c9222026da42230bc1fc9
karina quezada <quezadacore32@gmail.com>
2021-09-19 00:13:16 -0400

    US4

---
+++ src/test/java/com/LibraryCT/US4/AC1.java
@@ -0,0 +1,14 @@
+package com.LibraryCT.US4;
+
+public class AC1 {
+
+    /*
+    User Story 4
+As a librarian, I want to logout from the library homepage.
+AC #1:
+Given user is on the homePage
+When user click username on the right top corner
+And user click Log Out
+Then verify user navigate back to login page.
+     */
+}
