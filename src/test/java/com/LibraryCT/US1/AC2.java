939e54e7d636293cd2230862607bf51d55b4b244
karina quezada <quezadacore32@gmail.com>
2021-09-19 00:13:49 -0400

    US1

---
+++ src/test/java/com/LibraryCT/US1/AC2.java
@@ -0,0 +1,14 @@
+package com.LibraryCT.US1;
+
+public class AC2 {
+
+    /*
+    AC#2:
+Given student is on the loginPage
+Then verify that the URL is
+ “https://library2.cybertekschool.com/login.html”
+When student enters valid email address and password
+And student click sign in button
+Then verify that there are 2 models on the page
+     */
+}
